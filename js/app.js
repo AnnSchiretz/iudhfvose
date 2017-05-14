6419414cc4684ec5dcb5abdded20a0ebbdc5844f
AnnSchiretz <sisyrinchium02@gmail.com>
2017-05-14 20:13:45 +0300

    MFC

---
+++ js/app.js
@@ -0,0 +1,24 @@
+/**
+ * Created by hanna on 14.5.17.
+ */
+(function(){
+    $(document).ready(function(){
+        $('.btn-success').click(function(){
+            let textNotification = $('.to-do-control').val();
+            $('.to-do').append('<li><span class="content">' + textNotification + '</span> <i class="fa fa-times" aria-hidden="true"></i></li>');
+            $('.to-do-control').val('');
+        });
+
+        $('ul').on('click', 'li>span.content', function () {
+            if ($(this).hasClass('done')) {
+                $(this).removeClass('done');
+            } else {
+                $(this).addClass('done');
+            }
+        });
+
+        $('ul').on('click' , "li>.fa-times", function() {
+            $(this).parent().remove();
+        })
+    });
+})();
